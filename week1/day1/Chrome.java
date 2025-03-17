1a5fdc9e13f56087f909dcd80ca9ac18c08407c5
Suriyakala <63659021+suriyakala-2020@users.noreply.github.com>
2025-03-18 00:00:19 +0530

    Day1 completed assignment

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+
+public class Chrome {
+
+	public static void main(String[] args) {
+
+float version=91.0f;
+String developer="Google";
+Boolean beta=false;
+int releaseYear=2008;
+char shortcutKey='C';
+System.out.println("Version : "+version);
+System.out.println("Developer : "+developer);
+System.out.println("Beta : "+beta);
+System.out.println("Release Year : "+releaseYear);
+System.out.println("Shortcut Key : "+shortcutKey);
+	}
+}
